dc3bc13fee198892fa2ad9838742213e7ed1a44b
HaoWang95 <alanwang0028@gmail.com>
2022-04-10 12:14:24 +1000

    login and register update

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,11 @@
+from unicodedata import name
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home', views.HomeView.as_view(), name='home.index'),
-    path('home/authorized', views.AuthorizedView.as_view())
+    path('', views.HomeView.as_view(), name='home.index'),
+    path('home/authorized', views.AuthorizedView.as_view()),
+    path('login', views.LoginInterfaceView.as_view(), name='home.login'),
+    path('logout', views.LogoutInterfaceView.as_view(), name='home.logout'),
+    path('register', views.SignupView.as_view(), name='home.register')
 ]
